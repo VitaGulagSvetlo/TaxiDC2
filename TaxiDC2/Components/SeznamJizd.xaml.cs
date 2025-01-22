b3e578a45994f39cdd83b6ebcbcc0b5157e644a5
VitaGulagSvetlo <vit.svetelsky@outlook.cz>
2025-01-22 15:13:41 +0100

    .,.,,..

--- TaxiDC2/Components/SeznamJizd.xaml.cs
+++ TaxiDC2/Components/SeznamJizd.xaml.cs
@@ -28,7 +28,7 @@ namespace TaxiDC2
         private async void OnBackButtonPressed(object sender, EventArgs e)
         {
 
-            Shell.Current.GoToAsync($"{nameof(SeznamJizd)}");
+            //Shell.Current.GoToAsync($"{nameof(SeznamJizd)}");
 
         }
 
