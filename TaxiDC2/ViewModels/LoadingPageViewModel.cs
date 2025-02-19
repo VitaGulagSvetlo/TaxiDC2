339860a041ace6ab51721a01bc55e75cf492adc5
peno <peno@advisor-soft.com>
2025-02-19 21:52:39 +0100

    Dispatcher

--- TaxiDC2/ViewModels/LoadingPageViewModel.cs
+++ TaxiDC2/ViewModels/LoadingPageViewModel.cs
@@ -11,13 +11,13 @@ using TaxiDC2.Components.Login;
 
 namespace TaxiDC2.ViewModels
 {
-	public partial class LoadingPageViewModel: BaseViewModel
+	public partial class LoadingPageViewModel : BaseViewModel
 	{
 		private readonly FirebaseAuthClient _authClient;
 		private readonly IServiceProvider _serviceProvider;
 		private readonly IBiometric _biometricService;
 
-		public LoadingPageViewModel(FirebaseAuthClient authClient,IServiceProvider serviceProvider, IBiometric biometricService)
+		public LoadingPageViewModel(FirebaseAuthClient authClient, IServiceProvider serviceProvider, IBiometric biometricService)
 		{
 			_authClient = authClient;
 			_serviceProvider = serviceProvider;
@@ -25,7 +25,7 @@ namespace TaxiDC2.ViewModels
 			CheckUser();
 		}
 
-		private async void  CheckUser()
+		private async void CheckUser()
 		{
 			if (string.IsNullOrWhiteSpace(_authClient?.User?.Info?.Email))
 			{
@@ -43,30 +43,44 @@ namespace TaxiDC2.ViewModels
 			}
 			else
 			{
-					//var biometricStatus = await _biometricService.GetAuthenticationStatusAsync();
-					//// Handle biometricStatus based on the application's logic
+				var biometricStatus = await _biometricService.GetAuthenticationStatusAsync();
+				// Handle biometricStatus based on the application's logic
 
-					//if (biometricStatus != BiometricHwStatus.Success)
-					//	return;
+				if (biometricStatus != BiometricHwStatus.Success)
+				{
+					await Shell.Current.DisplayAlert("Error", "biometricStatus is false", "Close");
+					
+					return;
+				}
 
-					//var authenticationRequest = new AuthenticationRequest
-					//{
-					//	AllowPasswordAuth = true, // A chance to fallback to password auth
-					//	Title = "Authenticate", // On iOS only the title is relevant, everything else is unused. 
-					//	Subtitle = "Please authenticate using your biometric data",
-					//	NegativeText = "Use Password", // if AllowPasswordAuth is set to true don't use this it will throw an exception on Android
-					//	Description = "Biometric authentication is required for access",
-					//	AuthStrength = AuthenticatorStrength.Strong // Only relevant on Android
-					//};
+				var authenticationRequest = new AuthenticationRequest
+				{
+					AllowPasswordAuth = true, // A chance to fallback to password auth
+					Title = "Authenticate", // On iOS only the title is relevant, everything else is unused. 
+					Subtitle = "Please authenticate using your biometric data",
+					//NegativeText = "Use Password", // if AllowPasswordAuth is set to true don't use this it will throw an exception on Android
+					Description = "Biometric authentication is required for access",
+					AuthStrength = AuthenticatorStrength.Strong // Only relevant on Android
+				};
 
-					//var authenticationResponse = await _biometricService.AuthenticateAsync(authenticationRequest, CancellationToken.None);
-					//if (authenticationResponse.Status != BiometricResponseStatus.Success)
-					//{
-					//	return;
-					//}
+				//tady je to v dispatcheru, protoze jinak to bezelo v jinem tasku nez UI
+				//a vyhazovalo to nesmyslne chyby v Android java kodu
+				// rozchozeno nahodou a nevim co to presne dela :-)
+				Shell.Current.Dispatcher.Dispatch(async () =>
+				{
+					var authenticationResponse = await _biometricService.AuthenticateAsync(authenticationRequest, CancellationToken.None);
+					if (authenticationResponse.Status != BiometricResponseStatus.Success)
+					{
+						await Shell.Current.DisplayAlert("Error", authenticationResponse.ErrorMsg, "Close");
+						return;
+					}
+					else
+					{
+						Shell.Current.FlyoutHeader = new FlyoutHeaderControl(_authClient);
+						await Shell.Current.GoToAsync($"///{nameof(MainPage)}");
+					}
+				});
 
-				Shell.Current.FlyoutHeader = new FlyoutHeaderControl(_authClient);
-				await Shell.Current.GoToAsync($"///{nameof(MainPage)}");
 			}
 		}
 	}
